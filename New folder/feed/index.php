9d3e3d6eb6ffbe2e4495eedce9689bc9d99d5b32
katyyong <yongyt@siswa.um.edu.my>
2017-05-31 14:39:06 +0800

    New

---
+++ New folder/feed/index.php
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	>
+
+<channel>
+	<title>my10k.Adsplo</title>
+	<atom:link href="http://my10k.adsplo.com/feed/" rel="self" type="application/rss+xml" />
+	<link>http://my10k.adsplo.com</link>
+	<description>My10k with Adsplo</description>
+	<lastBuildDate>Sat, 27 May 2017 04:42:32 +0000</lastBuildDate>
+	<language>en-US</language>
+	<sy:updatePeriod>hourly</sy:updatePeriod>
+	<sy:updateFrequency>1</sy:updateFrequency>
+	<generator>https://wordpress.org/?v=4.7.5</generator>
+
+<image>
+	<url>http://my10k.adsplo.com/wp-content/uploads/2017/05/favicon-100x100.png</url>
+	<title>my10k.Adsplo</title>
+	<link>http://my10k.adsplo.com</link>
+	<width>32</width>
+	<height>32</height>
+</image> 
+	<item>
+		<title>Hello world!</title>
+		<link>http://my10k.adsplo.com/2017/05/17/hello-world/</link>
+		<comments>http://my10k.adsplo.com/2017/05/17/hello-world/#comments</comments>
+		<pubDate>Wed, 17 May 2017 07:26:21 +0000</pubDate>
+		<dc:creator><![CDATA[admin]]></dc:creator>
+				<category><![CDATA[Uncategorized]]></category>
+
+		<guid isPermaLink="false">http://my10k.adsplo.com/?p=1</guid>
+		<description><![CDATA[Welcome to WordPress. This is your first post. Edit or delete it, then start writing!]]></description>
+				<content:encoded><![CDATA[<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+]]></content:encoded>
+			<wfw:commentRss>http://my10k.adsplo.com/2017/05/17/hello-world/feed/</wfw:commentRss>
+		<slash:comments>1</slash:comments>
+		</item>
+	</channel>
+</rss>
